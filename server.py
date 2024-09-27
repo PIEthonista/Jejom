ff5f8d63e99d445537d8305cd723d6c09c18bab4
debbiechoong <u2102817@siswa.um.edu.my>
2024-09-27 10:50:23 +0900

    jubensha route added

--- server.py
+++ server.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 from llama_index.core import Settings
 from llama_index.llms.upstage import Upstage
 from llama_index.embeddings.upstage import UpstageEmbedding
-
+from scripts.script import ScriptGenerator 
+from scripts.script import Translator
 
 
 app = Flask(__name__)
@@ -36,6 +37,98 @@ pipeline = PipelineV2(
 @app.route('/')
 def home():
     return 'Hello World'
+
+
+@app.route('/generate_script', methods=['POST'])
+def generate_script():
+    try:
+        start_time = time.time()
+        # Get the JSON data from the request
+        data = request.json
+
+        # Extract the required fields from the JSON body
+        characters_num = data.get('characters_num')
+        cafe_name = data.get('cafe_name')
+        cafe_environment = data.get('cafe_environment')
+
+        # Ensure the required fields are provided
+        if not characters_num or not cafe_name or not cafe_environment:
+            return jsonify({"error": "Missing required fields"}), 400
+
+        # Create an instance of ScriptGenerator
+        script_generator = ScriptGenerator(
+            characters_num=characters_num,
+            cafe_name=cafe_name,
+            cafe_environment=cafe_environment
+        )
+
+        # Run tasks and generate script
+        output_json_path, cafe_name = script_generator.run_tasks()
+
+        translator = Translator(api_key=UPSTAGE_API_KEY)
+
+        
+        input_directory = "output_folder"
+
+        input_file_path = os.path.join(input_directory, "script.json")
+
+        output_file_path = os.path.join(input_directory, "translated_output.json")
+
+        translator.translate_and_save(input_file_path, output_file_path)
+        print(f"[Generate Trip took {time.time() - start_time} secs]")
+
+
+        return 'Generated'
+
+    except Exception as e:
+        return jsonify({"error": f"Error generating script: {e}"}), 500
+
+app.route('/script', methods=['POST'])
+def display_script():
+
+    input_directory = "output_folder"
+
+    input_file_path = os.path.join(input_directory, "output.json")
+
+    with open(input_file_path, 'r') as file:
+        script = json.load(file)
+        script_planner = script["Script Planner"]
+        character_designer = script["Character Designer"]
+        script_writer = script["Script Writer"]
+        clue_generator = script["Clue Generator"]
+        player_instruction_writer = script["Player Instruction Writer"]
+        title = script["Title"]
+        duration = script["Duration"]
+    
+    return jsonify({'Script Planner': script_planner, "Character Designer": character_designer,"Script Writer": script_writer, "Clue Generator":clue_generator, "Player Instruction Writer":player_instruction_writer, "Title":title, "Duration":duration})
+
+    
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    
+    input_directory = "output_folder"
+
+    output_file_path = os.path.join(input_directory, "translated_output.json")
+
+
+    try:
+        # Call the translate_and_save method of the Translator class
+        
+        with open(output_file_path, 'r') as file:
+            script = json.load(file)
+            script_planner = script["Script Planner"]
+            character_designer = script["Character Designer"]
+            script_writer = script["Script Writer"]
+            clue_generator = script["Clue Generator"]
+            player_instruction_writer = script["Player Instruction Writer"]
+            title = script["Title"]
+            duration = script["Duration"]
+        
+        return jsonify({'Script': script_planner, "Character Designer": character_designer,"Script Writer": script_writer, "Clue Generator":clue_generator, "Player Instruction Writer":player_instruction_writer, "Title":title, "Duration":duration}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 @app.route('/check_init_input', methods=['POST'])
 def check_init_input():
